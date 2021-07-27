e655e9515a14616b7f9dbc3324f43549ade7d4d3
MatTaNg <ng5280@hotmail.com>
2021-07-26 21:06:52 -0400

    3b

--- sevenSignal/stringCalculator.js
+++ sevenSignal/stringCalculator.js
@@ -1,11 +1,12 @@
 const stringCalculator = (input) => {
+    const comma_OR_newLine = /,|\n/;
     if(input === "") {
         return 0;
     }
-    if(input.search(/,|\n/) === -1) {
+    if(input.search(comma_OR_newLine) === -1) {
         return parseInt(input);
     }
-    const input_array = input.split(/,|\n/);
+    const input_array = input.split(comma_OR_newLine);
     return input_array.reduce((acc, cv) => parseInt(acc) + parseInt(cv))
 }
 
